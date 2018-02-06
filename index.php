4207694632df336c44b077cf9946b31c3725e2d9
simonecutri <simone.cutri1@gmail.com>
2018-02-05 22:47:37 -0500

    maybe

---
+++ index.php
@@ -0,0 +1,20 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title> Chess</title>
+</head>
+<body>
+
+hello
+<?php
+
+	$letter = $_GET["letter"];
+	$number = $_GET["number"];
+	$counter = 1;
+
+	echo $letter . "" $number;
+	?>
+
+	goodbye
+</body>
+</html>
