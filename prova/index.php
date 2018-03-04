dc3e51ded36440c12381e5a3da0c80fd9c0bc333
simonecutri <simone.cutri1@gmail.com>
2018-03-03 23:22:36 -0500

    yesssssssss

--- prova/index.php
+++ prova/index.php
@@ -3,6 +3,19 @@
 
 <head>
   <title>Fruit</title>
+  <style type="text/css">
+
+  @keyframes example {
+    from {filter: hue-rotate(20deg);}
+    to {filter: hue-rotate(300deg);}
+    }
+
+  .one {
+    animation-name: example;
+    animation-duration: 20s;
+  }
+
+  </style>
 <head>
 
 <div style=""> </div>
@@ -14,7 +27,6 @@
     <?php
     $letter = $_GET['letter'];
     $number = $_GET['number'];
-    $number = $number . 'deg';
 
     $table = ['a','b','c','d','e','f'];
     $TableLength = count($table, COUNT_RECURSIVE);
@@ -22,14 +34,27 @@
     $counter = 1;
     while ($counter <= $number){
 
+        if($number <= 100 ){
+          $className = "one";
+        }else if($number > 100 || $number <= 200 ){
+          $className = "due";
+        }else if($number > 200 || $number <= 400){
+          $className = "tre";
+        };
+
+
         for($i = 0 ; $i < $TableLength ; $i++){
       
             if( $letter != $table[$i] ){
-              echo "<img src='$table[$i].png'; style='filter: hue-rotate($number);'>";
+              echo "<img src='$table[$i].png' class='one'>";
             } else {
-               echo "<img src='$table[$i].png'; style='filter: hue-rotate($number);'>";
-                break;
+               echo "<img src='$table[$i].png' class='one'>";
+               break;
             };
+
+            usleep(100000);
+            flush();
+
     };
 
      $counter++;}
